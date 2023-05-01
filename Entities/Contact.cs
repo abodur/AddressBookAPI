448ce9e8de17789a2425c587fca05655389e446c
Atakan Bodur <atakanbodur@gmail.com>
2023-05-01 17:44:40 +0300

    Scaffolded db context

---
+++ Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookAPI.Entities;
+
+public partial class Contact
+{
+    public int ContactId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? MobilePhone { get; set; }
+
+    public string? Email { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookAPI.Entities;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public virtual ICollection<Contact> Contacts { get; set; } = new List<Contact>();
+}
