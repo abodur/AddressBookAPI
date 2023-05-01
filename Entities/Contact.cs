b137b025eb140c3a61b075c4b3aca04df46afa20
Atakan Bodur <atakanbodur@gmail.com>
2023-05-01 18:38:44 +0300

    Scaffold again

---
+++ Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookAPI.Entities;
+
+public partial class Contact
+{
+    public int ContactId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string? MobilePhone { get; set; }
+
+    public string? Email { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using AddressBookAPI.Entities;
+using MySql.EntityFrameworkCore.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddEntityFrameworkMySQL().AddDbContext<AddressBookContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
